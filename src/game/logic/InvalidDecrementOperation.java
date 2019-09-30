593a9521efc1d0b89fa4705b22607abe3571a6c8
Daudi Williams <47124964+DKWN83@users.noreply.github.com>
2019-09-30 14:25:54 -0500

    Here you go Reeder you Peter

---
+++ src/game/logic/InvalidDecrementOperation.java
@@ -0,0 +1,7 @@
+// package game.logic; 
+
+// class InvalidDecrementOperation extends Exception {
+//     public InvalidDecrementOperation(String s) {
+//         super(s);
+//     }
+// }
--- src/game/logic/PlayerLogic.java
+++ src/game/logic/PlayerLogic.java
@@ -3,14 +3,14 @@ package game.logic;
 public class PlayerLogic {
 
     protected Board gameBoard;
-    protected Player Player1 = new Player(PlayerToken.Player1, 9);
-    protected Player Player2 = new Player(PlayerToken.Player2, 9);
+    protected Player Player1 = new Player(PlayerToken.PLAYER1, 9);
+    protected Player Player2 = new Player(PlayerToken.PLAYER2, 9);
     protected Player activePlayer = Player1;
 
 public boolean placePlayerPiece(int index) {
     int pieces;
     if (gameBoard.getCell(index).isEmpty()) {
-        gameBoard.setCell(index, activePlayer.getToken());
+        gameBoard.setCell(index, activePlayer.getPlayerToken());
         activePlayer.decrementPiecesLeft();
         pieces = activePlayer.getPiecesLeft();
         isPhaseOneOver(pieces);
@@ -18,7 +18,7 @@ public boolean placePlayerPiece(int index) {
     }
     return false;
 }
-public static void nextTurn() {
+public void nextTurn() {
     if(activePlayer == Player1)
     {
         activePlayer = Player2;

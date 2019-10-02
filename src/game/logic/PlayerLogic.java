19e0b1418e1e2052c39db418c75e9e2fd6e1e283
Reed <reedh1997@yahoo.com>
2019-10-02 13:01:01 -0500

    added getActivePlayer()

--- src/game/logic/PlayerLogic.java
+++ src/game/logic/PlayerLogic.java
@@ -3,23 +3,26 @@ package game.logic;
 public class PlayerLogic {
 
     protected Board gameBoard;
-    protected Player Player1 = new Player(PlayerToken.Player1, 9);
-    protected Player Player2 = new Player(PlayerToken.Player2, 9);
+    protected Player Player1 = new Player(PlayerToken.PLAYER1, 9);
+    protected Player Player2 = new Player(PlayerToken.PLAYER2, 9);
     protected Player activePlayer = Player1;
 
-public boolean placePlayerPiece(int index) {
+public boolean placePlayerPiece(int index) {   //boolean for if player piece can be placed
     int pieces;
     if (gameBoard.getCell(index).isEmpty()) {
-        gameBoard.setCell(index, activePlayer.getToken());
-        activePlayer.decrementPiecesLeft();
-        pieces = activePlayer.getPiecesLeft();
-        isPhaseOneOver(pieces);
+        gameBoard.setCell(index, activePlayer.getPlayerToken());
+        activePlayer.decrementPiecesLeft();   //decrements the pieces left for current player
+        pieces = activePlayer.getPiecesLeft();   //sets the pieces left
         return true;
     }
     return false;
 }
-public static void nextTurn() {
-    if(activePlayer == Player1)
+public PlayerToken getActivePlayer() {   //call this to get current player token
+    return activePlayer;
+}
+
+public void nextTurn() {   //finds next turn
+    if(activePlayer == Player1)   //if current player token is player 1, then set to player 2, vice versa
     {
         activePlayer = Player2;
     }
@@ -29,7 +32,7 @@ public static void nextTurn() {
     }
 }
 
-    public boolean isPhaseOneOver(int pieces) {
+    public boolean isPhaseOneOver(int pieces) {  //check if phase one is over yet
         if(pieces > 0) {
             return false;
         }

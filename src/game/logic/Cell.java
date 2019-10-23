be9db7a626a9e307e6cfab09f9f31ef95f1a35c7
mikelhomeless <mthomer1027@gmail.com>
2019-10-22 20:22:51 -0500

    added functionality

--- src/game/logic/Cell.java
+++ src/game/logic/Cell.java
@@ -1,15 +1,50 @@
 package game.logic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Cell {
     PlayerToken player;
+    List<Integer> adjacentCells;
+
 
     public Cell(){
-        this.player = PlayerToken.NOPLAYER;
+        init();
     }
 
-    public Cell(PlayerToken player){
-        this.player = player;
+    /**
+     * Add a single cell index to the list of adjacent cells
+     *
+     * @param index
+     */
+    public void addAdjacentCell(Integer index) { this.adjacentCells.add(index); }
+
+    /**
+     * Obtain player token of currently occupied player
+     *
+     * @return Player occupying cell - PlayerToken
+     */
+    public PlayerToken getPlayer(){
+        return this.player;
     }
+
+    /**
+     * Private method for common steps in initializing a cell
+     */
+    private void init() {
+        this.player = PlayerToken.NOPLAYER;
+        this.adjacentCells = new ArrayList<>();
+    }
+
+    /**
+     * Public function to test if the cell is adjacent to another cell
+     *
+     * @param index
+     * @return true if the index is in the adjacentCells list, false otherwise
+     */
+    public boolean isAdjacentTo(int index) { return this.adjacentCells.contains(index); }
 
     /**
      * Function to determine if a cell is empty
@@ -40,21 +75,19 @@ public class Cell {
     }
 
     /**
+     * Append a list of cell indexes to the list of adjacent cells
+     *
+     * @param newAdjacentCells
+     */
+    public void setAdjacentCells(Integer[] newAdjacentCells) { this.adjacentCells.addAll(Arrays.asList(newAdjacentCells)); }
+
+    /**
      * Set value of player occupying the cell
      *
      * @param player - Player to be placed
      */
     public void setPlayer(PlayerToken player){
         this.player = player;
-    }
-
-    /**
-     * Obtain player token of currently occupied player
-     *
-     * @return Player occupying cell - PlayerToken
-     */
-    public PlayerToken getPlayer(){
-        return this.player;
     }
 
     /* Main function for if this class is being ran as main. This class should hardly ever be ran as main.

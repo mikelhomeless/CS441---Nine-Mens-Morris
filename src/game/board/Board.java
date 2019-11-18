8b8add7a7ca39cc6efcca4aa53c03a469bcc6b8b
mikelhomeless <mthomer1027@gmail.com>
2019-11-17 18:36:06 -0600

    refactored board

--- src/game/board/Board.java
+++ src/game/board/Board.java
@@ -1,7 +1,9 @@
 package game.board;
 
+import game.logic.Player;
 import game.logic.PlayerToken;
 import java.util.*;
+import java.util.stream.IntStream;
 
 public abstract class Board {
     private List<Cell> cells = new ArrayList<>();
@@ -27,6 +29,8 @@ public abstract class Board {
         }
     }
 
+    // Build the board up from the number of cells given.
+    // The setUp function is expected to be used to set up adjacencies and mill combinations
     public Board() {
         for (int i = 0; i < numberOfCells(); i++){
             cells.add(new Cell());
@@ -36,6 +40,12 @@ public abstract class Board {
         setUp();
     }
 
+    /**
+     * Adds a list of cell indexes to the adjacency list of the called upon cell
+     *
+     * @param cellIndx
+     * @param cells
+     */
     protected void addAdjacentCells(int cellIndx, Integer[] cells) {
         this.cells.get(cellIndx).addAdjacentCells(cells);
     }
@@ -44,25 +54,12 @@ public abstract class Board {
      * Returns an array of PlayerTokens stored within each cell on the board
      * NOTE: This function does not return the individual cells of the board, just the PlayerTokens stored within them
      *
-     * @return PlayerToken[]
+     * @return List of PlayerTokens
      */
-    public PlayerToken[] getBoard() {
-        PlayerToken[] board_rep = new PlayerToken[24];
-        for (int i = 0; i < cells.size(); i++) {
-            board_rep[i] = cells.get(i).getPlayer();
-        }
-        return board_rep;
-    }
-
-    /**
-     * Search through the game board to locate all cells that are empty
-     * <p>
-     * NOTE: alias for getCellsOccupiedBy(PlayerToken.NOPLAYER)
-     *
-     * @return (array)List of all cells that are empty
-     */
-    public List<Cell> getEmptyCells() {
-        return getCellsOccupiedBy(PlayerToken.NOPLAYER);
+    public List<PlayerToken> getBoard() {
+        List<PlayerToken> boardRep = new ArrayList<>();
+        cells.forEach(cell -> boardRep.add(cell.getPlayer()));
+        return boardRep;
     }
 
     /**
@@ -76,9 +73,15 @@ public abstract class Board {
         return cells.get(index);
     }
 
+    /**
+     * Searches through the game board to locate all cells the given player occupies
+     *
+     * @param player
+     * @return List of cells occupied by the given player in terms of their indexes on the board
+     */
     public List<Integer> getCellsAsIndexOccupiedBy(PlayerToken player) {
         List<Integer> ownedCells = new ArrayList<>();
-        for (int i = 0; i < 24; i++) {
+        for (int i = 0; i < cells.size(); i++) {
             if (cells.get(i).isOccupiedBy(player)) {
                 ownedCells.add(i);
             }
@@ -90,7 +93,7 @@ public abstract class Board {
      * Search through the game board to locate all cells the given player occupies
      *
      * @param player
-     * @return (Array)List of cells occupied by the given player
+     * @return List of cells occupied by the given player
      */
     public List<Cell> getCellsOccupiedBy(PlayerToken player) {
         List<Cell> ownedCells = new ArrayList<>();
@@ -101,6 +104,12 @@ public abstract class Board {
         return ownedCells;
     }
 
+    /**
+     * Get the count of the number of pieces the given player has on the board
+     *
+     * @param player
+     * @return Number of cells occupied by the player
+     */
     public int getCount(PlayerToken player) {
         int cnt = 0;
         for (Cell cell : cells) {
@@ -110,32 +119,59 @@ public abstract class Board {
         return cnt;
     }
 
-    public boolean moveFromTo(int srcIndex, int destIndex, PlayerToken player) {
-        if (getCell(destIndex).isOccupied())
-            return false;
-        try {
-            setCell(destIndex, player); //set dest = source cell, get index, make sure both indices exist
-            setCell(srcIndex, PlayerToken.NOPLAYER);
-        } catch (Exception ArrayIndexOutOfBoundsException) {
-            return false;
-        }
-        return true;
+    /**
+     * Search through the game board to locate all cells that are empty
+     *
+     * NOTE: alias for getCellsOccupiedBy(PlayerToken.NOPLAYER)
+     *
+     * @return (array)List of all cells that are empty
+     */
+    public List<Cell> getEmptyCells() {
+        return getCellsOccupiedBy(PlayerToken.NOPLAYER);
     }
 
-    public boolean removePieceFromCell(int index) {
-        if (cells.get(index).isEmpty()) {
-            return false;
-        }
-        cells.get(index).setPlayer(PlayerToken.NOPLAYER);
-        return true;
-    }
-
+    /**
+     * Check if the given cell is currently located in a mill
+     *
+     * @param index
+     * @return true if in a mill, false otherwise
+     */
     public boolean isCellInMill(int index) {
         for (Mill mill : cells.get(index).getMillCombinations()) {
             if (mill.isMillFormed())
                 return true;
         }
         return false;
+    }
+
+    /**
+     * Move a piece from one location to another
+     *
+     * @param srcIndex
+     * @param destIndex
+     * @return true if the operation was successfully carried out, false otherwise
+     */
+    public boolean moveFromTo(int srcIndex, int destIndex) {
+        if ( cells.get(srcIndex).isEmpty() || cells.get(destIndex).isOccupied() )
+            return false;
+
+        setCell(destIndex, cells.get(srcIndex).getPlayer());
+        setCell(srcIndex, PlayerToken.NOPLAYER);
+        return true;
+    }
+
+    /**
+     * Remove a piece from the board at the given cell index
+     *
+     * @param index
+     * @return true if the piece was removed, false otherwise
+     */
+    public boolean removePieceFromCell(int index) {
+        if (cells.get(index).isEmpty()) {
+            return false;
+        }
+        cells.get(index).setPlayer(PlayerToken.NOPLAYER);
+        return true;
     }
 
     /**
@@ -148,6 +184,13 @@ public abstract class Board {
         cells.get(index).setPlayer(player);
     }
 
+    /**
+     * Set a mill combination across three cells (as index)
+     *
+     * @param one
+     * @param two
+     * @param three
+     */
     protected void setMillCombination(int one, int two, int three) {
         Mill newMill = new Mill(cells.get(one), cells.get(two), cells.get(three));
         millCombinations.add(newMill);
